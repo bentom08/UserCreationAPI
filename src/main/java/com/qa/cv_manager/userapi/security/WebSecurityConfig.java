fe87d55a37e04e18818a975aebd01b914aa21b88
Bentom08 <bentom06@hotmail.co.uk>
2018-12-12 14:47:30 +0000

    added spring security for enable/disable and delete methods

--- src/main/java/com/qa/cv_manager/userapi/security/WebSecurityConfig.java
+++ src/main/java/com/qa/cv_manager/userapi/security/WebSecurityConfig.java
@@ -1,20 +1,40 @@
 package com.qa.cv_manager.userapi.security;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	DataSource dataSource;
+	
+	@Autowired
+	PasswordEncoder passwordEncoder;
+	
+	@Autowired
+     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.jdbcAuthentication().dataSource(dataSource).passwordEncoder(passwordEncoder)
+		.usersByUsernameQuery("select username,password, enabled from users where username=?")
+		.authoritiesByUsernameQuery("select username, role from user_roles where username=?");
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-        	.antMatchers("/deleteUser/**").hasRole("ADMIN")
-        	.anyRequest().permitAll()
+        http.csrf().disable()
+        	.httpBasic()
         	.and()
-        	.csrf().disable();
+        	.authorizeRequests()
+        	.antMatchers("/deleteUser/**", "/enableAccount/**", "/disableAccount/**").hasRole("ADMIN")
+        	.anyRequest()
+        	.authenticated();
     }
 }
